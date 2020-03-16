4772235b1cc42692bf9f7017e5f84c45199761da
kealanheena <kealanheena@gmail.com>
2020-03-16 23:27:47 +0000

    Chore: add calculator class

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  state = {
+    // value to be displayed in <Display />
+    displayValue: '0',
+    // values to be displayed in number <Keys />
+    numbers: [],
+    // values to be displayed in operator <Keys />
+    opperators: [],
+    // operator selected for math operation
+    selectedOperator: '',
+    // stored value to use for math operation
+    storedValue: ''
+  }
+
+  callOperator = () => {
+    console.log('Call Operator!');
+  }
+
+  setOperator = () => {
+    console.log('Set Operator!');
+  }
+
+  updateDisplay = () => {
+    console.log('Update Display!');
+  }
+
+  render = () => {
+    return (
+      <div className="calculator-container"/>
+    );
+  }
+}
+
+export default Calculator;
