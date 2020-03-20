0fe6c1b78e6d1d84039e55316ad7442b1f79697a
kealanheena <kealanheena@gmail.com>
2020-03-20 20:54:30 +0000

    Chore: update styling

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Key.css';
+import './Key.css'
 
 const Key = ({ keyAction, keyType, keyValue }) => (
   <div className="key-container">
